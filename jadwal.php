be6e28608d3d26da724a5da6a737d63a487883e8
DriBoy <stefanus080806@gmail.com>
2025-12-14 17:41:19 +0700

    jadwal.php yang bagus

--- jadwal.php
+++ jadwal.php
@@ -7,22 +7,21 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.5/css/dataTables.bootstrap5.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="https://cdn.datatables.net/2.3.5/js/dataTables.js"></script>
-    <script src="https://cdn.datatables.net/2.3.5/js/dataTables.bootstrap5.js"></script>
+    <!-- SweetAlert2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css"
+    rel="stylesheet">
+    <!-- SweetAlert2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </head>
 <body class="bg-light">
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <?php
         $conn = new mysqli("localhost","root","","siamu");
     ?>
     <h3 align="center" class="pt-4">Jadwal Kuliah Semester Gasal 2025/2026</h3>
     <div class="container pt-4 bg-white">
-        <table border="1" align="center" style="border-collapse: collapse;" id="example" class="table table-striped">
+        <table border="1" align="center" style="border-collapse: collapse;" id='example' class="table table-striped">
             <thead class="table-dark">
                 <tr>
                     <th>Hari</th>
@@ -45,7 +44,7 @@
                             echo "<td>".$brs['sks']."</td>";
                             echo "<td>".$brs['nama_ruang']."</td>";
                             echo "<td>".$brs['nama']."</td>";
-                            echo "<td><a href='edit_jadwal.php?id_jadwal=".$brs['id_jadwal']."'>Edit</a> | <a href='hapus_jadwal.php?id_jadwal=".$brs['id_jadwal']."'>Hapus</a></td>";
+                            echo "<td><button type='button' class='btn btn-warning me-1' onclick=\"editJadwal({$brs['id_jadwal']})\">Edit</button><button type='button' class='btn btn-danger' onclick=\"hapusJadwal(event, '{$brs['id_jadwal']}')\">Hapus</button></td>";
                             echo "</tr>";
                         }
                     ?>
@@ -55,13 +54,41 @@
     <div class="text-center pt-4">
         <button class="btn btn-primary" onclick="tambahJadwal()">Tambahkan Jadwal Kuliah</button>
     </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/2.3.5/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.3.5/js/dataTables.bootstrap5.js"></script>
     <script>
         function tambahJadwal() {
             window.location.href = "tambah_jadwal.php";
+        };
+        function editJadwal(id_jadwal) {
+            window.location.href = "edit_jadwal.php?id_jadwal=" + id_jadwal;
+        };
+        function hapusJadwal(event, id_jadwal) {
+            event.preventDefault(); // Mencegah halaman langsung pindah
+
+            Swal.fire({
+            title: "Yakin ingin menghapus data ini?",
+            text: "Data yang dihapus tidak dapat dikembalikan!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Ya, hapus!",
+            cancelButtonText: "Batal"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location.href = "hapus_jadwal.php?id_jadwal=" + id_jadwal;
+                }
+            });
         }
-        new DataTable('#example');
+        new DataTable('#example', {
+            paging: true,
+            pagingType: "simple_numbers"
+        });
     </script>
 </body>
 </html>
